bd0591a24c30282e6c8016f0e81ee171bde5885c
13-Rob <rcgr.13@gmail.com>
2023-04-06 11:37:41 -0700

    Correct and wrong answer

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correcAnswerSprite;
 
     void Start()
     {
@@ -18,6 +22,25 @@ public class Quiz : MonoBehaviour
         {
             buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        int correctAnswerIndex = question.GetCorrectAnswerndex();
+
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correcAnswerSprite;
+        }
+        else
+        {
+            questionText.text = "Sorry, the correct answer is\n " + question.GetAnswer(correctAnswerIndex);
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correcAnswerSprite;
         }
     }
 }
