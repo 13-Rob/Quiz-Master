844e0d0ee78d745522f8676782038eea6aa2178d
13-Rob <rcgr.13@gmail.com>
2023-04-10 09:30:48 -0700

    Timer logic

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float timeToAnswer = 30f;
+    [SerializeField] float timeToShowAnswer = 10f;
+    public bool loadNext;
+    public bool isAnswering = true;
+    public float fillFraction;
+    float timerValue;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (timerValue > 0)
+        {
+            fillFraction = isAnswering ? timerValue / timeToAnswer : timerValue / timeToShowAnswer;
+        }
+        else
+        {
+            isAnswering = !isAnswering;
+
+            if (isAnswering)
+                timerValue = timeToAnswer;
+            else
+            {
+                timerValue = timeToShowAnswer;
+                loadNext = true;
+            }
+        }
+
+        Debug.Log(fillFraction);
     }
 }
